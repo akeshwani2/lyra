3a58ef326919e7aeec3b3a6cc71261d9b5f709b6
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2024-12-02 21:06:05 -0500

    Completed landing page UI

--- src/components/ui/Testimonials.tsx
+++ src/components/ui/Testimonials.tsx
@@ -7,9 +7,15 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 const testimonials = [
   {
+    text: "“Love what Arhaan has done here. The impact was immediate - it's transformed how I work!”",
+    name: "Arya Toufanian",
+    title: "Founder/CEO of Cita Marketplace",
+    avatarImg: avatar2,
+  },
+  {
     text: "“The user interface is so intuitive and easy to use, it helps me organize my notes and tasks efficiently.”",
     name: "Maria Victoria",
-    title: "Upcoming student @ Stanford",
+    title: "Upcoming student @ FSU",
     avatarImg: avatar3,
   },
   {
@@ -25,7 +31,7 @@ const testimonials = [
     avatarImg: avatar1,
   },
   {
-    text: "“Since I began using Lyra, my time management has improved remarkably.",
+    text: "“Since I began using Lyra, my time management has improved remarkably.”",
     name: "Alec Whitten",
     title: "Upcoming student @ MIT",
     avatarImg: avatar4,
@@ -40,42 +46,27 @@ export const Testimonials = () => {
           Beyond Expectations.
         </h2>
         <p className="text-white/70 md:text-xl text-lg max-w-sm mx-auto text-center mt-5 tracking-tight">
-          Our innovative AI productivity tools have significantly enhanced our clients' workflows
+        Our revolutionary AI-powered tools are redefining how students and professionals organize, learn, and achieve their goals.
         </p>
-        <div className="overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <motion.div 
-            initial={{ translateX: '0%'}}
-            animate={{ translateX: '-50%'}}
+        <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div
+            initial={{ translateX: "-50%" }}
+            animate={{ translateX: "0%" }}
             transition={{
               duration: 30,
-              repeat: Infinity,
-              repeatType: "loop",
               ease: "linear",
-              repeatDelay: 0
+              repeat: Infinity,
             }}
-            className="flex gap-5 w-fit"
+            className="flex gap-5 pr-5 flex-none"
           >
-            {testimonials.map((testimonial) => (
-              <div key={testimonial.name} className="border-white/15 border p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
-                <div className="text-lg tracking-tight md:text-2xl">{testimonial.text}</div>
-                <div className="flex items-center gap-3 mt-5">
-                  <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgba(140,69,244,0.7)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
-                    <Image
-                      src={testimonial.avatarImg}
-                      alt=""
-                      className="h-11 w-11 rounded-lg grayscale border border-white/30"
-                    />
-                  </div>
-                  <div className="">
-                    <div>{testimonial.name}</div>
-                    <div className="text-white/50 text-sm">{testimonial.title}</div>
-                  </div>
+            {[...testimonials, ...testimonials].map((testimonial, index) => (
+              <div
+                key={`${testimonial.name}-${index}`}
+                className="border-white/15 border p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
+              >
+                <div className="text-lg tracking-tight md:text-2xl">
+                  {testimonial.text}
                 </div>
-              </div>
-            ))}
-            {testimonials.map((testimonial) => (
-              <div key={`${testimonial.name}-duplicate`} className="border-white/15 border p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none">
-                <div className="text-lg tracking-tight md:text-2xl">{testimonial.text}</div>
                 <div className="flex items-center gap-3 mt-5">
                   <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgba(140,69,244,0.7)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
                     <Image
@@ -86,7 +77,9 @@ export const Testimonials = () => {
                   </div>
                   <div className="">
                     <div>{testimonial.name}</div>
-                    <div className="text-white/50 text-sm">{testimonial.title}</div>
+                    <div className="text-white/50 text-sm">
+                      {testimonial.title}
+                    </div>
                   </div>
                 </div>
               </div>
